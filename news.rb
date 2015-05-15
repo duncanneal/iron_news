8f5dce2e30c788e13f6ee3c3e8966fd7adbee1c7
Duncan Neal <duncan.neal@gmail.com>
2015-05-15 12:18:15 -0400

    Commit

--- news.rb
+++ news.rb
@@ -8,8 +8,9 @@ ROOT = File.dirname(__FILE__)
 server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => "#{ROOT}/public")
 
 
-server.mount_proc '/' do |req, res|
-  @data = YAML.load_file('data.yml')
+server.mount_proc '/iron_news' do |req, res|
+  @page_title = "News"
+  @data = YAML.load_file("#{ROOT}/data.yml")
   template = ERB.new(File.read("#{ROOT}/index.html.erb"))
   res.body = template.result
 end
