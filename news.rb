f2a78bf5204ed78a7bf1d7ba84cd1675e6391268
Duncan Neal <duncan.neal@gmail.com>
2015-05-15 10:04:00 -0400

    Intial Commit

---
+++ news.rb
@@ -0,0 +1,21 @@
+
+require 'erb'
+require 'webrick'
+require 'yaml'
+
+ROOT = File.dirname(__FILE__)
+
+server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => "#{ROOT}/public")
+
+
+server.mount_proc '/' do |req, res|
+  @data = YAML.load_file('data.yml')
+  template = ERB.new(File.read("#{ROOT}/index.html.erb"))
+  res.body = template.result
+end
+
+trap 'INT' do
+  server.shutdown
+end
+
+server.start
